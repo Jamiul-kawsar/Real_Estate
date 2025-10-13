0f6fd9c8d458d5c7f90f6b9b2e645a547c2250c0
Jamiul Kawsar <jamiul.kawsar35@gmail.com>
2025-10-13 08:41:34 +0600

    add and design about section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,9 +22,9 @@ const Navbar = () => {
         <img src={assets.logo} alt="" />
         <ul className='hidden md:flex gap-8 text-white'>
             <a href="#Header" className='cursor-pointer hover:text-gray-400' >Home</a>
-            <a href="#Header" className='cursor-pointer hover:text-gray-400' >About</a>
-            <a href="#Header" className='cursor-pointer hover:text-gray-400' >Projects</a>
-            <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+            <a href="#About" className='cursor-pointer hover:text-gray-400' >About</a>
+            <a href="#Projects" className='cursor-pointer hover:text-gray-400' >Projects</a>
+            <a href="#Testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
         <img onClick={()=> setShowMobileMenu(true)} src={assets.menu_icon} className='md:hidden w-7 cursor-pointer'alt="" />
@@ -44,7 +44,7 @@ const Navbar = () => {
         </div>
         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
           <a onClick={()=> setShowMobileMenu(false)} href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
-          <a onClick={()=> setShowMobileMenu(false)} href="#Header" className='px-4 py-2 rounded-full inline-block'>About</a>
+          <a onClick={()=> setShowMobileMenu(false)} href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
           <a onClick={()=> setShowMobileMenu(false)} href="#Header" className='px-4 py-2 rounded-full inline-block'>Projects</a>
           <a onClick={()=> setShowMobileMenu(false)} href="#Header" className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
         </ul>
