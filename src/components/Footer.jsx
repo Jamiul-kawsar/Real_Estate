08fed9fbad9a704728cb7f6ab8daf84bdd50f161
Jamiul Kawsar <jamiul.kawsar35@gmail.com>
2025-10-21 08:47:21 +0600

    add footer section and design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,30 @@ const Footer = () => {
                 <img src={assets.logo_dark} alt="" />
                 <p className='text-gray-400 mt-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam libero earum quis dicta doloribus illum. Blanditiis nulla quaerat modi ratione praesentium minus voluptatibus eum tempore eligendi eos, ullam, labore maiores!</p>
             </div>
-            <div></div>
-            <div></div>
+            <div className='w-full md:w-1/5 mb-8 md:mb-0'>
+                <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
+                <ul className='flex flex-col gap-2 text-gray-400'>
+                    <a href="#Header" 
+                    className='hover:text-white'>Home</a>
+                    <a href="#About" 
+                    className='hover:text-white'>About Us</a>
+                    <a href="#Contact" 
+                    className='hover:text-white'>Contact Us</a>
+                    <a href="#" 
+                    className='hover:text-white'>Privacy Policy</a>
+                </ul>
+            </div>
+            <div className='w-full md:w-1/3 '>
+                <h3 className='text-white text-lg font-bold mb-4'>Subscribe to our newsletter</h3>
+                <p className='text-gray-400 mb-4 max-w-80'>The latest news, articles, and resources, sent to your inbox weekly.</p>
+                <div className='flex gap-2 '>
+                    <input type="email" placeholder='Enter Your Email' className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus: outline-none w-full md:w-auto' />
+                    <button className='px-4 py-2 rounded bg-blue-500'>Subscribe</button>
+                </div>
+            </div>
         </div>
-        <div>
-            <div></div>
+        <div className='border-t border-gray-700 py-4 mt-10 text-center text-gray-500'>
+          Copyright 2025 © Real-Estate-Jamiul. All rights reserved.
         </div>
     </div>
   )
